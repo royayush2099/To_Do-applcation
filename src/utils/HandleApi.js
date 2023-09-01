19952ab978990840c5eb2c1c3cec3e7ea47483fd
AYUSH ROY <112387986+royayush2099@users.noreply.github.com>
2023-09-01 21:16:56 +0530

    Add files via upload
    adding frontend

---
+++ src/utils/HandleApi.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+const baseUrl ="https://to-do-app-backend-cx2e.onrender.com"
+
+const getAllToDo = (setToDo)=>{
+    axios
+    .get(baseUrl)
+    .then(({data})=>{
+        console.log('data --->',data);
+        setToDo(data)
+    })
+}
+
+const addToDo =(text,setText,setToDo)=>{
+axios
+.post(`${baseUrl}/save`,{text})
+.then((data)=>{
+    console.log(data);
+    setText("")
+    getAllToDo(setToDo)
+})
+.catch((err)=> console.log(err))
+}
+
+const updateTodo =(toDoId,text,setToDo,setText,setIsUpdating)=>{
+    axios
+    .post(`${baseUrl}/update`,{_id:toDoId,text})
+    .then((data)=>{
+        setText("")
+        setIsUpdating(false)
+        getAllToDo(setToDo)
+        
+       
+    })
+    .catch((err)=> console.log(err))
+}
+    const deleteToDo=(_id,setToDo)=>{
+        axios
+        .post(`${baseUrl}/delete`,{_id})
+       
+        .then((data)=>{
+            console.log(data)
+            getAllToDo(setToDo)
+            
+           
+        })
+        .catch((err)=> console.log(err))
+    }
+export {getAllToDo,addToDo,updateTodo,deleteToDo}
